37f7523821421ad5b6a31076d3c6b2364a029b8f
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2023-03-17 01:42:22 +0300

    feat(Header): add Header layout

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SubmitHandler,useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
-import { SearchWrapper } from './styles';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import {
+    CloseElasticContainer,
+    ElasticContainer,
+    ElasticItem,
+    Form,
+    Input,
+    SearchWrapper,
+    SubmitButton,
+} from './styles';
 
 const Search = () => {
-    return <SearchWrapper />;
+    const [showElasticContainer, setShowElasticContainer] = useState(true);
+    const schema = yup
+        .object({
+            search: yup.string().required().min(1),
+        })
+        .required();
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors },
+    } = useForm<{ search: string }>({
+        resolver: yupResolver(schema),
+        defaultValues: { search: '' },
+    });
+    const formSubmit: SubmitHandler<{ search: string }> = data => {
+        console.log(data);
+    };
+    return (
+        <SearchWrapper>
+            <Form action="" onSubmit={handleSubmit(formSubmit)}>
+                <Input
+                    type="text"
+                    {...register('search', {
+                        onChange: handleSubmit(formSubmit),
+                    })}
+                    onFocus={() => {
+                        if (!showElasticContainer) {
+                            setShowElasticContainer(true);
+                        }
+                    }}
+                    placeholder="Serach..."
+                    name="search"
+                    id="search"
+                />
+                <SubmitButton
+                    disabled={Object.keys(errors).length > 0}
+                    type="submit"
+                    value="Search">
+                    Search
+                </SubmitButton>
+                {showElasticContainer && (
+                    <ElasticContainer>
+                        <ElasticItem
+                            onClick={() => {
+                                setShowElasticContainer(false);
+                                setValue('search', 'Minsk');
+                            }}>
+                            Minsk
+                        </ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <ElasticItem>Minsk</ElasticItem>
+                        <CloseElasticContainer
+                            onClick={() => {
+                                setShowElasticContainer(false);
+                            }}>
+                            Close
+                        </CloseElasticContainer>
+                    </ElasticContainer>
+                )}
+            </Form>
+        </SearchWrapper>
+    );
 };
 
 export default Search;
